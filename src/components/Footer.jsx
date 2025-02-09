d3346f1615c6a275b7606bca2e61cdd942002579
jamesfy2 <jameswkk52@gmail.com>
2025-02-09 20:07:45 +0700

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// Footer.jsx
+import React from 'react';
+
+const Footer = () => (
+  <div className="flex items-center justify-center mt-36 gap-4 text-sm bg-black p-4">
+    <h1 className="text-white">2025 | Designed and Coded with ❤ by Elli..</h1>
+  </div>
+);
+
+export default Footer;
